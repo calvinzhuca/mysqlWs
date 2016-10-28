259148f7bc2905bd8ea5e30ee0948fdc7a43f8a2
calvin.zhu <czhu@redhat.com>
2016-10-27 21:15:12 -0400

    test

--- product.js
+++ product.js
@@ -12,17 +12,13 @@ var dbHost = process.env.MYSQL_HOST,
       dbPassword = process.env.MYSQL_PASSWORD;
 
 
-
-
-//	dbHost = 'localhost';
-//	dbUser = 'product';
-//	dbDatabase = 'product';
-//	dbPassword = 'password';
-
 console.log('!!!!!!!!!!!!!!!!!!!!!!dbHost ' + dbHost);
 console.log('!!!!!!!!!!!!!!!!!!!!!!dbUser ' + dbUser);
 console.log('!!!!!!!!!!!!!!!!!!!!!!dbDatabase ' + dbDatabase);
 console.log('!!!!!!!!!!!!!!!!!!!!!!dbPassword ' + dbPassword);
+
+
+
 
 app.get('/', function(req, res) {
 	var result = [
@@ -31,34 +27,16 @@ app.get('/', function(req, res) {
   res.json(result);
 });
 
-app.get('/hello', function(req, res) {
+app.get('/version', function(req, res) {
 	var result = [
-	  { status : "hello 2"}
+	  { version : "33"}
 	];
   res.json(result);
 });
 
 
-
-
-
-
-
-app.post('/process', function(req, res) {
-  if(!req.body.hasOwnProperty('creditCardNumber') || !req.body.hasOwnProperty('verificationCode')) {
-    res.statusCode = 400;
-    return res.send('Error 400: need to have valid creditCardNumber and verificationCode.');
-  }
-
-
-	var date = new Date();
-	var year = date.getFullYear();
-	var month = date.getMonth() + 1;
-	var randomInt = (Math.random() * 9000000 + 1000000) | 0;
-
-	var result = null;
-
-
+//get either featured products or products with keyword
+app.get('/products', function(req, httpRes) {
 
 	var dbconn = mysql.createConnection({
 	  host     : dbHost,
@@ -66,6 +44,83 @@ app.post('/process', function(req, res) {
 	  password : dbPassword,
 	  database : dbDatabase
 	});
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+
+	if(req.query.featured == null && req.query.keyword == null) {
+		httpRes.statusCode = 400;
+		return httpRes.send('All products cannot be returned, need to provide a search condition');
+	}
+
+
+	if (req.query.featured != null) {
+		dbconn.query('SELECT * FROM PRODUCT where FEATURED = ?', req.query.featured, function(err, records){
+		  if(err) throw err;
+		  httpRes.json(records);
+		});
+	} else if (req.query.keyword != null){
+		dbconn.query('SELECT * FROM PRODUCT where SKU in (select SKU from PRODUCT_KEYWORD where KEYWORD = ?)', req.query.keyword, function(err, records){
+		  if(err) throw err;
+		  httpRes.json(records);
+		});
+	} 
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+});
+
+
+//get based on sku #
+app.get('/products/:sku', function(req, httpRes) {
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+	dbconn.query('SELECT * FROM PRODUCT where SKU = ?', req.params.sku, function(err, records){
+	  if(err) throw err;
+	  httpRes.json(records);
+	});
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+});
+
+
+//add product through post 
+app.post('/products', function(req, httpRes) {
+	if(!req.body.hasOwnProperty('DESCRIPTION') || !req.body.hasOwnProperty('NAME')) {
+		httpRes.statusCode = 400;
+		return httpRes.send('Error 400: need to have valid DESCRIPTION and NAME.');
+	}
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+
 
 	dbconn.connect(function(err){
 	  if(err){
@@ -75,27 +130,30 @@ app.post('/process', function(req, res) {
 	  }
 	});
 
-	dbconn.query('SELECT * FROM PRODUCT',function(err, records){
-	  if(err) throw err;
+	var record= { DESCRIPTION: req.body.DESCRIPTION, HEIGHT: req.body.HEIGHT, LENGTH: req.body.LENGTH,  NAME: req.body.NAME, WEIGHT: req.body.WEIGHT, WIDTH: req.body.WIDTH, FEATURED: req.body.FEATURED, AVAILABILITY: req.body.AVAILABILITY, IMAGE: req.body.IMAGE, PRICE: req.body.PRICE};
 
-//	  console.log(records);
-	  console.log(records[0].NAME);
 
-	result = [
-	  { status : "SUCCESS",
-	  name : records[0].NAME,
-	  orderNumber : req.body.orderNumber,
-	  transactionDate : date,
-	  transactionNumber : randomInt}
-	];
+	dbconn.query('INSERT INTO PRODUCT SET ?', record, function(err,dbRes){
+		if(err) throw err;
+		console.log('Last record insert SKU:', dbRes.insertId);
 
-	  res.json(result);
+		result = [
+		  { status : "SUCCESS",
+		  SKU : dbRes.insertId}
+		];
+
+		httpRes.json(result);
+
 
 	});
 
-
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
 
 });
+
+
 
 
 app.listen(process.env.PORT || 8080);
